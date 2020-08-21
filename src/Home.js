300903b5f1ef6fb18e70fbea0317179d24499889
Kanak Tyagi <tyagiknk.india@gmail.com>
2020-08-21 20:04:16 +0530

    Added the Home component with a background image

---
+++ src/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Home.css'
+
+function Home() {
+    return (
+        <div className="home">
+        <img className="home_image"
+        src="https://images-eu.ssl-images-amazon.com/images/G/31/AmazonVideo/2020/X-site/SingleTitle/BandishBandits/Launch/3000x1200_Hero-Tall_p._CB407155762_.jpg"
+        alt= "amazonimage" />
+            <h1> I am the home page!!!</h1>
+        </div>
+    )
+}
+
+export default Home
