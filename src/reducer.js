5d9afa3da2b81b6867eeb275bb844c7f66623f96
Kanak Tyagi <tyagiknk.india@gmail.com>
2020-08-24 15:33:26 +0530

    Added the Add to basket action functionality

--- src/reducer.js
+++ src/reducer.js
@@ -1,17 +1,21 @@
 export const initialState = {
     basket: [],
+    user:null,
 }
 
-function reducer(state, action) {
+const reducer =(state, action) => {
+    console.log(action);
     switch(action.type) {
-        case 'ADD_TO_BASKET': {
-            break;
-        }
-        case 'REMOVE_FROM_BASKET': {
-            break;
-        }
+        case 'ADD_TO_BASKET': 
+            return {
+                ...state,
+                basket: [...state.basket, action.item]
+            } 
+        case 'REMOVE_FROM_BASKET': 
+            return {state}
+            
         default:
-            return state
+            return{state}
     }
 }
 
