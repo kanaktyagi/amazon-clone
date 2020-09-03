c5df7d02f2d402d530256c0e9fd8dcec8176d4d3
Kanak Tyagi <tyagiknk.india@gmail.com>
2020-09-03 19:20:25 +0530

    Added the login name to be displayed dynamically when user sign in

--- src/Header.js
+++ src/Header.js
@@ -5,12 +5,18 @@ import './Header.css'
 import SearchIcon from '@material-ui/icons/Search'
 import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket'
 import {useStateValue} from './StateProvider'
+import { auth } from './firebase'
 
 function Header() {
   
-  const [{basket}] = useStateValue();
+  const [{basket,user}] = useStateValue();
 
-  console.log(basket);
+  const login =()=> {
+    if(user) {
+      auth.signOut();
+    }
+  }
+
 
     return (
         <nav className="header">
@@ -30,10 +36,10 @@ function Header() {
         {/*# Links */}
         <div className="header_nav">
 
-        <Link to="/login" className="header__link">
-        <div className="header__option"> 
-          <span className="header__optionLineOne"> Hello kanak</span>
-          <span className="header__optionLineTwo"> Sign In</span>
+        <Link to={!user && "/login"} className="header__link">
+        <div  onClick={login}className="header__option"> 
+          <span className="header__optionLineOne"> Hello {user?.email}</span>
+          <span className="header__optionLineTwo">{user ? 'Sign Out': 'Sign In'}</span>
         </div>
         </Link>
 
