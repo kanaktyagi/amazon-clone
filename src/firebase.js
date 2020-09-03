dbb4a81c4ac879e09c6d2527fbec01f00c70dda3
Kanak Tyagi <tyagiknk.india@gmail.com>
2020-09-03 15:45:14 +0530

    Added the firebase config for login Authentication

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import firebase from "firebase";
+
+const firebaseApp = firebase.initializeApp ({
+    apiKey: "AIzaSyAYpGNswzBRSUlAICPOWs675c69nOYVh6w",
+    authDomain: "clone-c3161.firebaseapp.com",
+    databaseURL: "https://clone-c3161.firebaseio.com",
+    projectId: "clone-c3161",
+    storageBucket: "clone-c3161.appspot.com",
+    messagingSenderId: "768273071660",
+    appId: "1:768273071660:web:9aeeda8c8da4debf621b89",
+    measurementId: "G-9ZRS2YW39S"
+  });
+  const auth = firebase.auth();
+
+  export {auth};
