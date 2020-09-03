0eb7fff35aeeb7c2ec9468bc301b12d6aa12dd1b
Kanak Tyagi <tyagiknk.india@gmail.com>
2020-09-03 19:23:16 +0530

    Dispatch the SET_USER action in the App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
-import React from 'react';
+import React,{useEffect} from 'react';
 import {BrowserRouter as Router, Switch, Route } from "react-router-dom"
 import './App.css';
 import Header from './Header';
 import Home from './Home';
 import Checkout from './Checkout';
+import Login from './Login'
+import {auth} from './firebase'
+import { useStateValue } from './StateProvider';
 
 function App() {
+  const [{basket}, dispatch] = useStateValue();
+
+  useEffect(() => {
+  const unSubscribe = auth.onAuthStateChanged(authuser =>{
+     if(authuser) {
+       //logged in
+       dispatch({
+         type: "SET_USER",
+         user: authuser
+       })
+     }else {
+       //logged out
+       dispatch({
+        type: "SET_USER",
+        user: null
+      })
+     }
+      return() =>{
+        unSubscribe()
+      }
+   })
+  }, [])
+
   return (
     <Router>
     <div className="app">
      <Switch>
+
      <Route path="/checkout">
      <Header/>
      <Checkout />
      </Route>
+
      <Route path="/login">
-     <h1>Login</h1>
+     <Login/>
      </Route>
+
      <Route path="/">
      <Header/>
      <Home />
      </Route>
+
      </Switch>
     </div>
     </Router>
