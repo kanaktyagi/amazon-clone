aee9f878697815267074e9545a555f221649b9ea
Kanak Tyagi <tyagiknk.india@gmail.com>
2020-09-03 15:30:35 +0530

    Added the selector in the reducer to caculate the item price

--- src/reducer.js
+++ src/reducer.js
@@ -2,6 +2,8 @@ export const initialState = {
     basket: [],
     user:null,
 }
+export const getBasketTotal = (basket) => 
+basket?.reduce((amount,item) => item.price + amount,0);
 
 const reducer =(state, action) => {
     console.log("action", action);
