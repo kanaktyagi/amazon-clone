aaa2bed6ff292d55404334f4fce4b8fdd7d63a78
Kanak Tyagi <tyagiknk.india@gmail.com>
2020-09-03 19:21:30 +0530

    Added the SET_USER action in the reducer

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,6 @@
 import firebase from "firebase";
 
-const firebaseApp = firebase.initializeApp ({
+const firebaseConfig= ({
     apiKey: "AIzaSyAYpGNswzBRSUlAICPOWs675c69nOYVh6w",
     authDomain: "clone-c3161.firebaseapp.com",
     databaseURL: "https://clone-c3161.firebaseio.com",
@@ -10,6 +10,7 @@ const firebaseApp = firebase.initializeApp ({
     appId: "1:768273071660:web:9aeeda8c8da4debf621b89",
     measurementId: "G-9ZRS2YW39S"
   });
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
   const auth = firebase.auth();
 
   export {auth};
--- src/reducer.js
+++ src/reducer.js
@@ -8,6 +8,11 @@ basket?.reduce((amount,item) => item.price + amount,0);
 const reducer =(state, action) => {
     console.log("action", action);
     switch(action.type) {
+        case 'SET_USER': 
+        return{
+            ...state,
+            user:action.user
+        }
         case 'ADD_TO_BASKET': 
             return {
                 ...state,
